abf769bbf535aa97b7786aab7c5442b5a5222998
pruthulpa07 <pruthulpa07@gmail.com>
2016-10-19 11:23:12 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,5 +4,5 @@ var counts = 0
 button.onClick = function(){
     counts = counts + 1 ;
   var span = document.getElementById('count');
-  span.innerTEXT = counts.toString();
+  span.innerHTML = counts.toString();
 }
