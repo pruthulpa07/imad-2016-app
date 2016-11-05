5d41dce25cbb796b56af74cd4d51112be3908015
pruthulpa07 <pruthulpa07@gmail.com>
2016-11-05 06:12:55 +0530

    Add files via upload

--- ui/main.js
+++ ui/main.js
@@ -1,8 +1,17 @@
-var button = document.getElementById('counter')
-var counts = 0
-
-button.onClick = function(){
-    counts = counts + 1 ;
-  var span = document.getElementById('count');
-  span.innerHTML = counts.toString();
+var button=document.getElementById('counter');
+var counter=0;
+button.onclick=function(){
+var request=new XMLHttpRequest();
+request.onreadystatechange=function(){
+if(request.readyState==XMLHttpRequest.DONE){
+    if(request.status==200){
+        var counter=request.responseText;
+         var element=document.getElementById('count');
+    element.innerHTML=counter.toString();
+        
+    }
+}
+    }
+request.open('GET','http://localhost:8080/counter',true);
+    request.send('null');
 }
